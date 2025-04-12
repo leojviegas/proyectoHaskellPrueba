c30ecaa86297d5c69e3d71e0aad7016c4e09ab3d
leojviegas <leojviegas@hotmail.com>
2025-04-12 00:53:59 -0300

    añado los archivos del proyecto de stack al repo remoto

---
+++ src/Lib.hs
@@ -0,0 +1,9 @@
+--esExceso:: int -> bool
+
+{- (esExceso velocidad) = (velocidad > 60)
+ def. de la funcion             codigo propiamente dicho       
+
+-} 
+
+esExceso :: Int -> Bool
+esExceso velocidad = velocidad > 60
